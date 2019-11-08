51ee71085e0b12fd21fbca5139c660f44168e5e1
hidez8891 <427357+hidez8891@users.noreply.github.com>
2019-11-09 00:11:13 +0900

    add native-base font load event

--- App.tsx
+++ App.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native";
+import * as Font from "expo-font";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StoreProvider } from "./Context";
 import ScreenRouter from "./ScreenRouter";
 
 export default function App() {
+  const [isReady, setReady] = useState<Boolean>(false);
+
+  async function NativeBaseInitialize() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+    });
+    setReady(true);
+  }
+
+  useEffect(() => {
+    if (!isReady) {
+      NativeBaseInitialize();
+    }
+  }, []);
+
+  if (!isReady) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <SafeAreaProvider>
       <StoreProvider>
